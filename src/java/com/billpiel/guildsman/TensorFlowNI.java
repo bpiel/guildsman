42fddd7dca7125312c30964e2ad4ccd89a989e20
Bill Piel <bpiel@rjmetrics.com>
2017-10-24 16:10:28 -0400

    load nat libs in TF NI java class

--- src/java/com/billpiel/guildsman/TensorFlowNI.java
+++ src/java/com/billpiel/guildsman/TensorFlowNI.java
@@ -4,6 +4,10 @@ public final class TensorFlowNI {
     public static native String version();
     public static native byte[] registeredOpList();
 
+    static {
+        NativeLibrary.load();
+    }
+
     public static int mask(int crc) {
         return ((crc >>> 15) | (crc << 17)) + 0xa282ead8;
     }
