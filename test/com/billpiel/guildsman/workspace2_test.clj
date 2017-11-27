7bc773a9ed71b634e571251ac32bccb3236128df
Bill Piel <bpiel@rjmetrics.com>
2017-11-27 12:13:50 -0500

    cleanup

--- test/com/billpiel/guildsman/workspace2_test.clj
+++ test/com/billpiel/guildsman/workspace2_test.clj
@@ -6,6 +6,32 @@
             [com.billpiel.guildsman.data-type :as dt]
             [com.billpiel.guildsman.dev :as dev]))
 
+
+(def color-idx
+  (zipmap [:red :orange :yellow :green :blue :purple :black :white]
+          (range)))
+
+(def train-data
+  [[[1. -1. -1.] (color-idx :red)]
+   [[1. 0. -1.] (color-idx :orange)]
+   [[1. 1. -1.] (color-idx :yellow)]
+   [[-1. 1. -1.] (color-idx :green)]
+   [[-1. -1. 1.] (color-idx :blue)]
+   [[1. -1. 1.] (color-idx :purple)]
+   [[-1. -1. -1.] (color-idx :black)]
+   [[1. 1. 1.] (color-idx :white)]])
+
+(def test-data
+  [[[0.9 -1. -0.8] (color-idx :red)]
+   [[1. 0.2 -1.] (color-idx :orange)]
+   [[0.8 1. -1.] (color-idx :yellow)]
+   [[-1. 0.9 -1.] (color-idx :green)]
+   [[-0.8 -1. 1.] (color-idx :blue)]
+   [[1. -0.9 1.] (color-idx :purple)]
+   [[-1. -0.8 -1.] (color-idx :black)]
+   [[0.8 0.9 1.] (color-idx :white)]])
+
+
 (g/def-workspace ws1
   (g/let+ [p1 (o/placeholder :p1 g/dt-float [1])
            {:keys [v a1 tr]}
